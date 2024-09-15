3df31adab489244bbba5a1ddb5db43670a45353e
pr-Dutta <89duttapranjal@gmail.com>
2024-09-15 20:06:34 +0530

    new commit

--- app/src/main/java/com/example/locationapp/LocationViewModel.kt
+++ app/src/main/java/com/example/locationapp/LocationViewModel.kt
@@ -5,10 +5,10 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 
 class LocationViewModel : ViewModel() {
-    private val _location = mutableStateOf<LocationData?>(null)
-    val location: State<LocationData?> = _location
-
-    fun updateLocation(newLocation: LocationData) {
-        _location.value = newLocation
-    }
+//    private val _location = mutableStateOf<LocationData?>(null)
+//    val location: State<LocationData?> = _location
+//
+//    fun updateLocation(newLocation: LocationData) {
+//        _location.value = newLocation
+//    }
 }
